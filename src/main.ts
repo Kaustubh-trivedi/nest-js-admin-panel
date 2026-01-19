fef4b4d4aeefd8198cf0f5c7c2479fd6ef043a93
Kaustubh-trivedi <kaustubh@alobhadev.com>
2026-01-20 00:01:01 +0530

    assets and public folder setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import expressLayouts from 'express-ejs-layouts';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.use(expressLayouts);
+  app.set('layout', 'index'); // default layout
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
